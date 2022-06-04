9b455500d1aed5f5c9d098d81edbadf0de37e91a
DErkan-A <80825222+DErkan-A@users.noreply.github.com>
2022-06-04 18:16:51 +0300

    Decreased speed

--- Node_model_Usrp.py
+++ Node_model_Usrp.py
@@ -163,7 +163,7 @@ def main():
     topo.construct_winslab_topology_with_channels(number_of_nodes, UsrpNode, FIFOBroadcastPerfectChannel)
   # topo.construct_winslab_topology_without_channels(number_of_nodes, UsrpNode)
     topo.start()
-    run_test(topo,0.001,number_of_nodes,100,5)
+    run_test(topo,0.05,number_of_nodes,100,5)
 
 
 if __name__ == "__main__":
