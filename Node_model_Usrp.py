a85a301af17ecc41f7cabb30ad2a9d5acee38f96
DErkan-A <80825222+DErkan-A@users.noreply.github.com>
2022-06-04 11:44:08 +0300

    small change

--- Node_model_Usrp.py
+++ Node_model_Usrp.py
@@ -162,7 +162,7 @@ def main():
     topo.construct_winslab_topology_without_channels(number_of_nodes, UsrpNode)
   # topo.construct_winslab_topology_with_channels(2, UsrpNode, FIFOBroadcastPerfectChannel)
     topo.start()
-    run_test(topo,0.1,number_of_nodes,500,5)
+    run_test(topo,0.1,number_of_nodes,200,5)
 
 
 if __name__ == "__main__":
