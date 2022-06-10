887a794f51631472d1ccf8335a7d19e39a775876
DErkan-A <80825222+DErkan-A@users.noreply.github.com>
2022-06-10 10:27:49 +0300

    small fix

--- Node_model_Usrp.py
+++ Node_model_Usrp.py
@@ -166,6 +166,7 @@ def main():
     topo.construct_winslab_topology_without_channels(number_of_nodes, UsrpNode)
     topo.start()
     run_test(topo,0.1,number_of_nodes,100,5)
+    topo.exit()
 
 
 if __name__ == "__main__":
