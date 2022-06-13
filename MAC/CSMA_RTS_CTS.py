f53798ee0ab74b8254b4c8044339fb471abc4404
DErkan-A <80825222+DErkan-A@users.noreply.github.com>
2022-06-13 13:53:51 +0300

    changed timings a little

--- MAC/CSMA_RTS_CTS.py
+++ MAC/CSMA_RTS_CTS.py
@@ -32,7 +32,7 @@ class ComponentConfigurationParameters():
     pass
 
 class MacCsmaRTS_CTS_ConfigurationParameters (ComponentConfigurationParameters):
-    def __init__(self, slot_time = 0.05, NAV_RTS = 0.15, NAV_CTS = 0.1, NAV_DATA = 0.05, message_threshold=100, cca_threshold = -35):
+    def __init__(self, slot_time = 0.05, NAV_RTS = 0.2, NAV_CTS = 0.15, NAV_DATA = 0.1, message_threshold=100, cca_threshold = -35):
         self.slot_time = slot_time
         self.NAV_RTS = NAV_RTS
         self.NAV_CTS = NAV_CTS
