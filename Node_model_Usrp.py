db4a044718987b64ded67985309819fe8e126c0f
DErkan-A <80825222+DErkan-A@users.noreply.github.com>
2022-06-13 15:23:56 +0300

    10 messages

--- Node_model_Usrp.py
+++ Node_model_Usrp.py
@@ -166,7 +166,7 @@ def main():
     #topo.construct_winslab_topology_with_channels(number_of_nodes, UsrpNode, FIFOBroadcastPerfectChannel)
     topo.construct_winslab_topology_without_channels(number_of_nodes, UsrpNode)
     topo.start()
-    run_test(topo,0.01,number_of_nodes,4,5)
+    run_test(topo,0.01,number_of_nodes,10,15)
 
 if __name__ == "__main__":
     main()
